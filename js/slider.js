06c54ff0bc8e9b64534df28d74f80ba89099e771
Chang-Hui <58720085+ckdgml5067@users.noreply.github.com>
2019-12-31 02:39:03 +0900

    2019.12.31 02:38
    두번째 수정

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,5 @@
 /*
-\제이쿼리
+ 제이쿼리
   $(선택자).실행문();
   반복분
   length
@@ -7,11 +7,34 @@
 
   }
 */
-$(document).ready(function () {
+$(document).ready(function slider() {
+  var number = 0;
   var slider = $('.visual li').length;
+  var $slider = $('.visual li')
+  var $prev = $('.prev');
+  var $next = $('.next');
   console.log(slider);
-  for (var i = 0; i < slider; i++) {
-    $eq(i).css({left: i*100+'%'});
-  console.log(i);
-  }
+
+  //이미지 개수 & 이미지 우측으로 정렬
+  for (var i = 0; i < slider; i++){
+    $('.visual li')[i].style.left = i*100 + '%';
+    console.log(i);
+  };
+
+  //우측 버튼 클릭시 이동
+  $next.click(function() {
+    $slider.eq(number).stop().animate({left:'-100%'});
+    number++;
+    number = number % slider;
+
+    $slider.eq(number).css({left:'100%'}).stop().animate({left:'0'});
+
+    play();
+  })
+
+  //좌측 버튼 클릭시 이동
+
 });
+
+
+// $('.visual li').eq(i).css({left: 'eq(i)' * 100 + '%'});
